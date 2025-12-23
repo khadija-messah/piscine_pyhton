38da1bed05af1c5ad19a712ddbdca67359b3243c
khadija messah <duva@MacBook-Pro-de-khadija.local>
2025-12-23 17:10:49 +0100

    ex01 done

--- D00/ex01/format_ft_time.py
+++ D00/ex01/format_ft_time.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime
 
 curence = time.time()
-print(f"Seconds since January 1, 1970: {curence:,} or {curence:.2e} in scientific notation")
+print(f"Seconds since January 1, 1970: {curence:,.4f} or {curence:.2e} in scientific notation")
 
 now = datetime.now()
 print(now.strftime("%b %d %Y"))
