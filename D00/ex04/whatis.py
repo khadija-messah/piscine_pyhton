8dee89b6f1fce452208af52d1f7778d67897d61f
khadija messah <duva@MacBook-Pro-de-khadija.local>
2025-12-27 13:17:15 +0100

    ex04 done

--- D00/ex04/whatis.py
+++ D00/ex04/whatis.py
@@ -1,18 +1,19 @@
 import sys
 
 def main():
-    if(len(sys.argv) < 2):
-        exit()
+    if(len(sys.argv)==1):
+        return
     if(len(sys.argv) != 2):
         print("AssertionError: more than one argument is provided")
     else:
         try:
             intiger = int(sys.argv[1])
-            if(isinstance(intiger,int)):
-                if(intiger % 2 == 0):
-                    print("I'm Even.")
-                else:
-                    print("I'm Odd")
         except:
             print("AssertionError: argument is not an integer")
-main()
+            return
+        if(intiger % 2 == 0):
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+if __name__ == '__main__':
+    main()
