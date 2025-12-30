671351d7fade905996cfddf2052de932297fed7c
Khadija Messah <khmessah@c4r1p6.1337.ma>
2025-12-30 21:07:21 +0100

    ex06 15%

--- D00/ex06/filterstring.py
+++ D00/ex06/filterstring.py
@@ -1,12 +1,26 @@
 
 from ft_filter import ft_filter
 
-
+def check_caractere(c):
+    if not c.isalpha():
+        return 1
+    return 0
 def parssing_N(intiger):
     try:
         n = int(intiger)
     except ValueError:
         print("argument should be intiger")
+        return 1
+
+def parssing_S(s):
+    try:
+        s1 = str(s)
+        arr = s.split()
+        for i in arr:
+            for s in i:
+                assert check_caractere(s),"machi caractere"
+    except AssertionError as error:
+        print(error)
         return 1
 import sys
 def filterstring():
@@ -14,6 +28,8 @@ def filterstring():
         assert len(sys.argv) == 3 ,"error number argmuent"
         if parssing_N(sys.argv[2]):
             return
+        if parssing_S(sys.argv[1]):
+            return 
     except AssertionError as error:
         print(error)
 filterstring()
