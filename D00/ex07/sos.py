6f2bccb815e8b6599593b32c47ea8e49988b9eac
Khadija Messah <khmessah@c4r1p8.1337.ma>
2026-01-02 20:01:04 +0100

    ex07 80% done

--- D00/ex07/sos.py
+++ D00/ex07/sos.py
@@ -1,5 +1,37 @@
 import sys
-def main():
 
+def data_dic(name):
+    dic = {'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-',' ':'/'}
+    for i in name:
+        if not i.isspace():
+            print(dic[i.upper()],end='')
+        else:
+            print(dic[i],end='')
+        print(" ",end='')
+    print()
+
+
+def check_caractere(arg):
+    for i in arg:
+        assert i.isdigit() or i.isalpha() or i.isspace() ,"error caractere" 
+def main():
     assert len(sys.argv) == 2 , "AssertionError: the arguments are bad"
-main()
+    check_caractere(sys.argv[1])
+    data_dic(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
