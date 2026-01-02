86234c0d7411f1d050dfaf125de7694c3d6e00b9
khadija messah <duva@MacBook-Pro-de-khadija.local>
2026-01-02 11:14:26 +0100

    ex06 done

--- D00/ex06/filterstring.py
+++ D00/ex06/filterstring.py
@@ -1,44 +1,18 @@
 
 import sys
 from ft_filter import ft_filter
-
-def check_caractere(c):
-    if not c.isalpha():
-        return 0
-    return 1
-def parssing_N(intiger):
+def main():
     try:
-        n = int(intiger)
-    except ValueError:
-        print("AssertionError: argument should be intiger")
-        return 1
-
-def parssing_S(s):
-    result = []
-    try:
-        s1 = str(s)
-        arr = s.split()
-        for i in arr:
-            for s in i:
-                assert check_caractere(s),"not caractere"
-            result.append(i)
-    except AssertionError as error:
-        print(error)
-        return 1
-    return result
-
-def filterstring():
-    try:
-        assert len(sys.argv) == 3 ,"AssertionError: error number argmuent"
-        if parssing_N(sys.argv[2]):
-            return
+        assert len(sys.argv) == 3
+        assert sys.argv[2].isdigit()
+        
         s = int(sys.argv[2])
-        res = parssing_S(sys.argv[1])
-        t = []
-        if res == 1:
-            return
-        li = print(list(ft_filter(lambda name : len(name) > s, res)))
+        res = sys.argv[1]
+        words = res.split()
+        li = list(ft_filter(lambda name : len(name) > s, words))
+        print(li)
     except AssertionError as error:
-        print(error)
-filterstring()
-print(ft_filter.__doc__)
+        print("AssertionError: the arguments are bad")
+
+if __name__ == "__main__":
+    main()
--- D00/ex06/ft_filter.py
+++ D00/ex06/ft_filter.py
@@ -5,9 +5,6 @@ def ft_filter(fun,iterab):
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.'''
-    arr = []
-    for i in iterab:
-        if fun(i):
-            arr.append(i)
-    it = iter(arr)
-    return it
+    if fun is None:
+        return iter([item for item in iterab if item])
+    return iter([item for item in iterab if fun(item)])
